d93cdfebb3454149cc6e43a5349f6ffdbe3905de
Cybrwire <32025428+Cybrwire@users.noreply.github.com>
2020-04-19 13:09:23 -0500

    Add files via upload

---
+++ login/login.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { auth } from 'firebase/app'
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  username: string = ""
+  password: string = ""
+
+  constructor(public afAuth: AngularFireAuth,
+    public router: Router) { }
+
+  ngOnInit() {
+  }
+
+
+  async login(){
+    const {username, password} = this;
+    try {
+        const res = await this.afAuth.signInWithEmailAndPassword(username + '@gmail.com', password);
+        this.router.navigate(['/tabs']);
+    } catch(err) {
+      console.dir(err);
+      if(err.code === 'auth/user-notfound') {
+        console.log("User not found");
+      }
+    }
+
+  }
+  
+  async logout(){
+    await this.afAuth.signOut();
+  }
+}
